1541077faed98c56199b1c81fc208024b4c16955
Lucas <lucasmorais582@gmail.com>
2021-03-04 13:53:22 -0300

    Estrutura inicial do projeto

---
+++ src/routes/index.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+// import postRouter from './posts.routes';
+// import redditRouter from './reddit.routes';
+
+const routes = Router();
+
+// routes.use('/posts', postRouter);
+// routes.use('/reddit', redditRouter);
+
+export default routes;
---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import app from './app';
+
+app.use('/test-connection', (request, response) =>{
+  return response.json("FALA MEU MANO.");
+});
+
+app.listen(3333, () => {
+  console.log('🚀 Server started on port 3333!');
+});
